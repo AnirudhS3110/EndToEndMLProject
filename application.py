5802d6988cff6946d64cf9f4ce857159ec6146a0
AnirudhS3110 <anirudh23bcs19@iiitkottayam.ac.in>
2025-11-25 00:28:48 +0530

    Commented application.rub()

--- application.py
+++ application.py
@@ -32,5 +32,5 @@ def predict_datapoint():
          return render_template('home.html',result= predicted_score[0])
 
 
-if __name__=="__main__":
-     application.run(host="0.0.0.0")     
+# if __name__=="__main__":
+#      application.run(host="0.0.0.0")     
